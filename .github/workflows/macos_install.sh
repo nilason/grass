b262d8265ff76f83bf935b52c7ff3a92f22746de
Nicklas Larsson <n_larsson@yahoo.com>
2024-02-18 12:45:52 +0100

    test

--- .github/workflows/macos_install.sh
+++ .github/workflows/macos_install.sh
@@ -69,7 +69,7 @@ export CPPFLAGS="-isystem${CONDA_PREFIX}/include"
 
 ./configure $CONFIGURE_FLAGS
 
-EXEMPT=""
+EXEMPT="-Wno-error=strict-prototypes"
 make -j$(sysctl -n hw.ncpu) CFLAGS="$CFLAGS -Werror $EXEMPT" \
   CXXFLAGS="$CXXFLAGS -Werror $EXEMPT"
 
