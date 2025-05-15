add94f22b1e69f377a3eea92c18ec9eab564ab10
Nicklas Larsson <n_larsson@yahoo.com>
2025-05-15 11:43:26 +0200

    debug 1

--- .github/workflows/print_versions.sh
+++ .github/workflows/print_versions.sh
@@ -13,3 +13,13 @@ grass --version
 grass --tmp-project XY --exec g.version -ergb
 # Detailed Python version info (in one line thanks to echo)
 grass --tmp-project XY --exec bash -c "echo Python: \$(\$GRASS_PYTHON -c 'import sys; print(sys.version)')"
+
+
+cat << EOF > test_r_mapcalc.sh
+#!/usr/bin/env bash
+
+g.gisenv set="DEBUG=3"
+r.mapcalc "foo = 1"
+EOF
+chmod +x test_r_mapcalc.sh
+grass --tmp-project XY --exec test_r_mapcalc.sh
